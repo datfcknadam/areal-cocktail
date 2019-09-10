2d429d7a4f1358787804da92fdb4f97870645feb
Kulikov Egor <benstiller68@gmail.com>
2019-09-10 16:01:56 +0300

    update

---
+++ src/components/Coctail-list/CocktailList.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="cocktail-list">
+    <div @change="importValue" v-for="a in cocktailIn"
+      :key="a.id"
+      class="cocktail">
+      <div class="name">
+        <p>{{a.name}} </p>
+      </div>
+      <div class="volume">
+        {{a.vol}}
+      </div>
+      <img :src="a.src">
+      <div class="alco">
+        <b>Крепость:</b> {{a.alco}}
+      </div>
+      <div class="price">
+        {{a.price}}
+      </div>
+      <div class="ingredient">
+        <p><b>Состав:</b> {{a.ingredient}}</p>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+
+export default {
+  name: 'CoctailList',
+  props: {
+    cocktailIn: {
+      type: Array,
+    },
+  },
+  methods: {
+    importValue() {
+      this.$emit('cocktail', this.cocktail);
+    },
+  },
+};
+
+</script>
+<style scoped>
+.name {
+  font-size: calc(1vw + 1vh);
+  font-weight: 600;
+}
+
+.price::after {
+  content: "руб."
+}
+
+.volume::after {
+  content: "ml";
+}
+
+.alco::after {
+  content: "%";
+}
+img {
+  height: 20vh;
+  padding-bottom: 10px;
+  padding-top: 10px;
+}
+</style>
---
+++ src/components/Navbar/Dropdown.vue
@@ -0,0 +1,34 @@
+<template>
+  <b-nav-dropdown lazy="true" :text="text" class="m-md-2">
+    <b-dropdown-item
+      v-for="value in item"
+      v-text="value.name"
+      :key="itemsKey + value.id"
+      href="#"
+      @click="click(value.value)"
+    />
+  </b-nav-dropdown>
+</template>
+<script>
+
+export default {
+  name: 'Dropdown',
+  props: {
+    text: String,
+    item: {
+      type: Array,
+    },
+    itemsKey: String,
+  },
+  data() {
+    return {
+    };
+  },
+  methods: {
+    click(value) {
+      this.$emit('click', value);
+    },
+  },
+};
+
+</script>
